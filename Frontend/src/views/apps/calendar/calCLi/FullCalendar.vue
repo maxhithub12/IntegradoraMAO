34490b0140c28245720796519c1eef38f84a3bed
maxhithub12 <yshanks7@gmail.com>
2024-04-18 17:10:00 -0600

    Agregacion de constantes en la vista

--- Frontend/src/views/apps/calendar/calCLi/FullCalendar.vue
+++ Frontend/src/views/apps/calendar/calCLi/FullCalendar.vue
@@ -4,3 +4,11 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+
+let eventGuid = 0
+let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
+const today = new Date();
+const y = today.getFullYear();
+const m = today.getMonth();
+const d = today.getDate();
