b741a519b9dcb19e744e837e278e36f91357e8d1
maxhithub12 <yshanks7@gmail.com>
2024-04-18 17:08:55 -0600

    Importacion de los componentes que se utilizaran para la vista

---
+++ Frontend/src/views/apps/calendar/calCLi/FullCalendar.vue
@@ -0,0 +1,6 @@
+<script>
+import { defineComponent } from 'vue'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
